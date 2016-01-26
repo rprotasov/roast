1c5d9d5d75e504b97100692787900ec101d3a420
Richard Protasov <richardprotasov@gmail.com>
2016-01-25 23:55:08 -0500

    Renames test for edge data structure

---
+++ test/test-edge.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+var DirectedEdge = require('../src/edge');
+
+describe('DirectedEdge', function() {
+    var h = 5,
+        t = 6,
+        w = 7;
+    var e = new DirectedEdge(
+    {   head: h,
+        tail: t,
+        weight: w
+    });
+
+    describe('#get_weight()', function() {
+        it('returns the edge weight', function() {
+            expect(e.get_weight()).to.equal(w);
+        });
+    });
+
+    describe('#get_of()', function() {
+        it('returns the vertex head', function() {
+            expect(e.get_of()).to.equal(h);
+        });
+    });
+
+    describe('#get_to()', function() {
+        it('returns the vertex tail', function() {
+            expect(e.get_to()).to.equal(t);
+        });
+    });
+});
